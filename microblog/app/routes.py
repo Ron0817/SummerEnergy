1e933350526e5f4b81fdb3e9045703d886af1d0c
Ron Du <rosen2234767652@gmail.com>
2023-07-16 00:41:14 -0400

    login

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,6 +1,8 @@
-from flask import render_template
+from flask import render_template, url_for
+from markupsafe import escape
 from app import app
 
+# Index page
 @app.route('/')
 @app.route('/index')
 def index():
@@ -16,3 +18,30 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+# Variables on url
+@app.route('/user/<username>')
+def show_user_profile(username):
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
+
+# url_for()
+
+# HTTP Methods
+from flask import request
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return 'do the login'
+    else:
+        return render_template('login.html', title='Login', login_url=url_for('login'))
