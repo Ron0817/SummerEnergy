7f75c89ec82197076b237b2feb8975b58fe89ebd
rondu <rosen2234767652@gmail.com>
2023-08-07 23:32:08 -0400

    memcache

--- myCloud/app/__init__.py
+++ myCloud/app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from app.memcache import Memcache
 
 global memcache
-memcache = Memcache()
+memcache = Memcache(capacity=10, replacement_policy=0)
 app = Flask(__name__)
 app.secret_key = 'myCloudSuperSecretKey'
 
--- myCloud/app/memcache.py
+++ myCloud/app/memcache.py
@@ -1,7 +1,24 @@
 class Memcache:
-    def __init__(self):
+    def __init__(self, capacity, replacement_policy):
         self.cache = {}
+        self.replacement_policy = replacement_policy
+        self.capacity = capacity
+        self.num_of_serve = 0
+        self.miss_rate = 0
+        self.hit_rate = 0        
 
+    def values(self):
+        return self.cache.values()
+
+    def keys(self):
+        return self.cache.keys()
+    
+    def items(self):
+        return self.cache.items()
+    
+    def itself(self):
+        return self.cache
+    
     def put(self, key, value):
         self.cache.update({key: value})
 
@@ -15,14 +32,6 @@ class Memcache:
     def invalidate_key(self, key):
         self.cache.pop(key)
     
-    def values(self):
-        return self.cache.values()
-
-    def keys(self):
-        return self.cache.keys()
+    def refresh_configuration():
+        return
     
-    def items(self):
-        return self.cache.items()
-    
-    def itself(self):
-        return self.cache
